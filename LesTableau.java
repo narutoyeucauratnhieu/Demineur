0269460d4ca60df9f4fb962e7b367ce1684b5100
narutoyeucauratnhieu <83649146+narutoyeucauratnhieu@users.noreply.github.com>
2021-06-03 11:27:36 +0200

    Commits sur LesTableau.java

--- LesTableau.java
+++ LesTableau.java
@@ -1,10 +1,10 @@
 import java.lang.Math;
 
 public class LesTableau {  
-    public LesTableau()
+    public LesTableau() //constructeurs
     {
     }
-    public char [][] generatePlayerMap(int N)
+    public char [][] generatePlayerMap(int N) //Le map initial du joueur
     {
         char [][] arr = new char [N][N];
         for (int i=0;i<arr.length;i++)
@@ -17,7 +17,7 @@ public class LesTableau {
         return arr;
     }
 
-    public char echangeTab(int [][]arr, int x, int y)
+    public char echangeTab(int [][]arr, int x, int y) //convert from a character to an int
     {
         char [][] ans = new char [arr.length][arr.length];
         ans[x][y] = (char) (arr[x][y] +'0');
@@ -39,13 +39,13 @@ public class LesTableau {
         {
             posX = (int) (Math.random()*N);
             posY = (int) (Math.random()*N);
-            if (ans[posX][posY] == true)
+            if (ans[posX][posY] == true) //si la nouvelle mine n'est pas dans la carte de check 
             {
                 ans[posX][posY] = false;
             }
-            else
+            else   //si la nouvelle mine est dans la carte de check
             {
-                while(ans[posX][posY] == false )
+                while(ans[posX][posY] == false ) // on regenerate la position verticale et horizontale des mines
                 {
                     posX = (int) (Math.random()*N);
                     posY = (int) (Math.random()*N);
@@ -56,17 +56,17 @@ public class LesTableau {
         return ans;
     }
 
-    public int [][]mines (boolean [][]check)
+    public int [][]mines (boolean [][]check) // on convert de un tableau de boolean à int
     {
         int [][]ans = new int [check.length][check.length];
-        for (int i=0;i<ans.length;i++)
+        for (int i=0;i<ans.length;i++) //initiale 
         {
             for (int j=0;j<ans[i].length;j++)
             {
                 ans[i][j] = 0;
             }
         }
-        for (int i=0;i<ans.length;i++)
+        for (int i=0;i<ans.length;i++) //if check[i][j] = false, on laisse ans[i][j] = -1
         {
             for (int j=0;j<ans[i].length;j++)
             {
@@ -79,7 +79,7 @@ public class LesTableau {
         return ans;
     }
 
-    public int [][] calculvosines(int [][]mines)
+    public int [][] calculvosines(int [][]mines) //on calcul les voisines du bombe 
     {
         int [][]arr = new int [mines.length][mines.length];
         for (int i=0;i<arr.length;i++)
@@ -97,62 +97,62 @@ public class LesTableau {
                 if (arr[x][y] == -1)
                 {
                     //On verifie les voisines du bomb
-                    if ((y >= 0 && y <= N-2) && (x >=0 && x <= N-1))
+                    if ((y >= 0 && y <= N-2) && (x >=0 && x <= N-1)) //center right
                     { 
                         if (arr[x][y+1] != -1)
                         {
                             arr[x][y+1] +=1;
                         }
-                    } //center right
-                    if ((y >= 1 && y <= N-1) && (x >=0 && x <= N-1))
+                    } 
+                    if ((y >= 1 && y <= N-1) && (x >=0 && x <= N-1))//center left
                     {
                         if (arr[x][y-1] != -1)
                         {
                             arr[x][y-1] +=1;
                         }
-                    } //center left
-                    if ((y >= 1 && y <= N-1) && (x >=1 && x <= N-1))
+                    } 
+                    if ((y >= 1 && y <= N-1) && (x >=1 && x <= N-1)) //top left
                     {
                         if (arr[x-1][y-1] != -1)
                         {
                             arr[x-1][y-1] +=1;
                         }
-                    } //top left
-                    if ((y >= 0 && y <= N-2) && (x >=1 && x <= N-1))
+                    } 
+                    if ((y >= 0 && y <= N-2) && (x >=1 && x <= N-1)) // top right
                     {
                         if (arr[x-1][y+1] != -1)
                         {
                             arr[x-1][y+1] +=1;
                         }
-                    } // top right
-                    if ((y >= 0 && y <= N-1) && (x >=1 && x <= N-1))
+                    } 
+                    if ((y >= 0 && y <= N-1) && (x >=1 && x <= N-1)) // top center
                     {
                         if (arr[x-1][y] != -1)
                         {
                             arr[x-1][y] +=1;
                         }
-                    } // top center
-                    if ((y >= 0 && y <= N-2) && (x >=0 && x <= N-2))
+                    } 
+                    if ((y >= 0 && y <= N-2) && (x >=0 && x <= N-2)) //bottom right
                     {
                         if (arr[x+1][y+1] != -1)
                         {
                             arr[x+1][y+1] +=1;
                         }
-                    } //bottom right
-                    if ((y >= 1 && y <= N-1) && (x >=0 && x <= N-2))
+                    } 
+                    if ((y >= 1 && y <= N-1) && (x >=0 && x <= N-2)) //bottom left
                     {
                         if (arr[x+1][y-1] != -1)
                         {
                             arr[x+1][y-1] +=1;
                         }
-                    } //bottom left
-                    if ((y >= 0 && y <= N-1) && (x >=0 && x <= N-2))
+                    } 
+                    if ((y >= 0 && y <= N-1) && (x >=0 && x <= N-2)) //bottom center
                     {
                         if (arr[x+1][y] != -1)
                         {
                             arr[x+1][y] +=1;
                         }
-                    } //bottom center
+                    } 
                 }
             }
         }
